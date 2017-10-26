b73b5d70ae00233877045b3ce48930508158c26b
Amulya Tharimana <Amulya Tharimana>
2017-10-26 14:26:03 -0400

    Add project files.

---
+++ Project1/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project1.Models
+{
+    public class Order
+    {
+        public string Name { get; set; }
+        public int order_no { get; set; }
+    }
+}
---
+++ Project1/Models/Phone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project1.Models
+{
+    public class Phone
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
